dc9971235ccee411c4ef1757313955257c870b61
kass kass <pradocchavezyojaida@gmail.com>
2025-10-21 11:08:39 -0500

    comienzo del proyecto

---
+++ GestorFinanciero/src/main/java/pe/edu/upeu/sysgfinanzas/GestorFinancieroApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.sysgfinanzas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFinancieroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorFinancieroApplication.class, args);
+	}
+
+}
---
+++ GestorFinanciero/src/test/java/pe/edu/upeu/gestorfinanciero/GestorFinancieroApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.gestorfinanciero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFinancieroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
